610baf4e40f1f3dd16acb49489fcb258a28d0ffd
truejasonfans <472408570@qq.com>
2018-08-12 15:15:36 +0800

    fixed requestAnimationFrame issue.

--- TinyHeart/src/js/fish_game.js
+++ TinyHeart/src/js/fish_game.js
@@ -6,37 +6,34 @@ import Fruit from './fruit_es6';
 import DataObj from './data_es6';
 import Background from './background.jpg'
 export default class FishGame {
-  constructor() {
+  constructor(window) {
     this.canvas1 = document.getElementById("canvas1");
     this.canvas2 = document.getElementById("canvas2");
     this.ctx1 = this.canvas1.getContext("2d");
     this.ctx2 = this.canvas2.getContext("2d");
-
+    this.window = window
     this.canvas1.addEventListener('mousemove',this.onMouseMove, false);
 
     this.canWidth = this.canvas1.width;
     this.canHeight = this.canvas1.height;
 
-    //this.ane = new Ane();
-    //this.data = new DataObj();
+    this.ane = new Ane();
+    this.data = new DataObj();
     
     this.bg = new Image();
     this.bg.src = Background;
     this.lastTime = 0;
-  
     this.oceaContext = new OceaContext(this.bg, this.canWidth, this.canHeight, this.ctx2);
-    //this.oceaContext.drawBackground();
-    this.gameloop();
- 
+    
   }
   onMouseMove(e) {
-    // if(!this.data.gameover && (e.offSetX || e.layerX)) {
-    //   mx = e.offSetX == undefined ? e.layerX:e.offSetX;
-    //   my = e.offSetY == undefined ? e.layerY:e.offSetY;
-    // }
+    if(!this.data.gameover && (e.offSetX || e.layerX)) {
+      mx = e.offSetX == undefined ? e.layerX:e.offSetX;
+      my = e.offSetY == undefined ? e.layerY:e.offSetY;
+    }
   }
   gameloop() {
-    window.requestAnimationFrame(this.gameloop());
+    requestAnimationFrame(this.gameloop.bind(this));
     var now = Date.now();
     this.deltaTime = now - this.lastTime;
     this.lastTime = now;
--- TinyHeart/src/js/main.js
+++ TinyHeart/src/js/main.js
@@ -4,13 +4,7 @@ import FishGame from './fish_game';
 
 document.body.onload = game;
 function game() {
-  let fishGame = new FishGame();
-  
-	//init();
-	// lastTime = Date.now();
-	// deltaTime = 0;
-
-
+	new FishGame().gameloop();
 }
 
 
