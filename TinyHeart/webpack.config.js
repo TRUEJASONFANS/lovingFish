4e89fee1c94f76192425bc81ec93eea71bb669ec
truejasonfans <472408570@qq.com>
2018-08-19 23:54:08 +0800

    update structrue.

--- TinyHeart/webpack.config.js
+++ TinyHeart/webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./src/js/main.js",
+  mode: 'production',
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title:'Output management'
+    })
+  ],
   module: {
     rules: [
       {
